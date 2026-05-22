324a4dc95ed9279c26af031ba2d85c4684ccad24
valentinciubuc4-cmyk <valentinciubuc4@gmail.com>
2026-05-22 02:05:21 +0100

    Change Home button to use SVG icon

--- app1.py
+++ app1.py
@@ -521,7 +521,9 @@ st.markdown("</div>", unsafe_allow_html=True)
 st.markdown('<div class="bottom-nav-wrapper">', unsafe_allow_html=True)
 nav_cols = st.columns(4)
 with nav_cols[0]:
-    if st.button(<i class="bi bi-house"></i> "Home", key="nav_home"):
+    if st.button(" <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+  <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
+</svg> Home", key="nav_home"):
         st.session_state["current_page"] = "Dashboard"
         st.rerun()
 with nav_cols[1]:
